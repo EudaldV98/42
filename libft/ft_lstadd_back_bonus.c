3055900b737f4cd0e810852b2b29f6362faf3d23
Martin GARCIA LOPEZ <mgarcia-@e3r11p15.42.fr>
2019-10-14 10:39:32 +0200

    lst thngs vol. II

---
+++ libft/ft_lstadd_back_bonus.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgarcia- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/14 09:37:58 by mgarcia-          #+#    #+#             */
+/*   Updated: 2019/10/14 10:38:47 by mgarcia-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **alst, t_list *new)
+{
+	t_list	*list;
+
+	list = *alst;
+	if (list)
+	{
+		while (list->next)
+			list = list->next;
+		list->next = new;
+		new->next = NULL;
+	}
+	else
+		*alst = new;
+}
---
+++ libft/ft_lstmap_bonus.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgarcia- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/14 10:18:51 by mgarcia-          #+#    #+#             */
+/*   Updated: 2019/10/14 10:38:49 by mgarcia-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, t_list *(*f)(void *))
+{
+	t_list	*begin_list;
+
+	begin_list = NULL;
+	while (lst)
+	{
+		ft_lstadd_back(&begin_list, (*f)(lst->content));
+		lst = lst->next;
+	}
+	return (begin_list);
+}
