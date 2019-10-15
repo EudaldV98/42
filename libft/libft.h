dd74d1ac83465be538d07f9572f2107e4435b02f
Martin <mgarcia-@student.42.fr>
2019-10-15 12:53:57 +0200

    indentation

--- libft/libft.h
+++ libft/libft.h
@@ -6,11 +6,11 @@
 
 typedef struct	    s_list
 {
-    void			*content;
-    struct s_list	*next;
-}					t_list;
+    void	    *content;
+    struct s_list   *next;
+}		    t_list;
 
-int		ft_atoi(const char *str);
+int	ft_atoi(const char *str);
 
 size_t	ft_strlen(const char *str);
 
@@ -20,9 +20,9 @@ char	*ft_strncat(char *restrict s1, const char *restrict s2, size_t n);
 
 size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t size);
 
-int		ft_strcmp(const char *str1, const char *str2);
+int	ft_strcmp(const char *str1, const char *str2);
 
-int		ft_strncmp(const char *str1, const char *str2, size_t n);
+int	ft_strncmp(const char *str1, const char *str2, size_t n);
 
 char	*ft_strcpy(char *dest, const char *src);
 
@@ -40,19 +40,19 @@ char	*ft_strchr(const char *s, int c);
 
 char	*ft_strrchr(const char *s, int c);
 
-int		ft_isalpha(int c);
+int 	ft_isalpha(int c);
 
-int		ft_isdigit(int c);
+int	ft_isdigit(int c);
 
-int		ft_isalnum(int c);
+int	ft_isalnum(int c);
 
-int		ft_isascii(int c);
+int	ft_isascii(int c);
 
-int		ft_isprint(int c);
+int	ft_isprint(int c);
 
-int		ft_toupper(int c);
+int	ft_toupper(int c);
 
-int		ft_tolower(int c);
+int	ft_tolower(int c);
 
 char	*ft_strrev(char *str);
 
@@ -68,7 +68,7 @@ void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 
 void	*ft_memchr(const void *s, int c, size_t n);
 
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
 
 void	*ft_memmove(void *dst, const void *src, size_t len);
 
@@ -104,7 +104,7 @@ t_list	*ft_lstnew(void *content);
 
 void	ft_lstadd_front(t_list **alst, t_list *new);
 
-int		ft_lstsize(t_list *lst);
+int	ft_lstsize(t_list *lst);
 
 t_list	*ft_lstlast(t_list *lst);
 
