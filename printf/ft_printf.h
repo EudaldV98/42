c98c164b47bb16c91dddffaea46ac075f5218c77
Martin GARCIA LOPEZ <mgarcia-@e3r11p15.42.fr>
2019-11-21 11:59:53 +0100

    printf

---
+++ printf/ft_printf.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgarcia- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/20 16:52:56 by mgarcia-          #+#    #+#             */
+/*   Updated: 2019/11/20 19:19:14 by mgarcia-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+#include <stdarg.h>
+#include <unistd.h>
+
+
+int		ft_printf(const char *format, ...);
+
+int		ft_vprintf(const char *format, va_arg ap);
+
+
+
+#endif
