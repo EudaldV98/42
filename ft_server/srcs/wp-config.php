7f60f69cb9814e155698fe2d5e724a5f6eec5911
Martin <mgarcia-@student.42.fr>
2020-06-13 01:21:50 +0200

    docker

---
+++ ft_server/srcs/wp-config.php
@@ -0,0 +1,8 @@
+/** The name of the database for WordPress */
+define('DB_NAME', 'wordpress');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', '');
