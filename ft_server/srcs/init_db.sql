77358312802432bbc33f495fd13f1b905db01457
Martin GARCIA LOPEZ <mgarcia-@e2r10p20.42.fr>
2020-06-13 05:25:39 +0200

    docker

--- ft_server/srcs/init_db.sql
+++ ft_server/srcs/init_db.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE wordpress;
 GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost';
 FLUSH PRIVILEGES;
-update mysql.user set plugin = 'mysql_native_password' where user='root';
+UPDATE mysql.user SET plugin = 'mysql_native_password', authentication_string = PASSWORD('root') WHERE User = 'root';
