ffef0b3469eb8b4b5f248a7570be3cb7dbdd6d80
Martin <mgarcia-@student.42.fr>
2020-07-22 08:16:43 +0200

    42 reservation system I hate u

--- ft_services/setup.sh
+++ ft_services/setup.sh
@@ -1,15 +1,19 @@
 # start minikube + addons + change docker enviroment to miniukbe docker
 
+#minikube start
+
+#eval $(minikube docker-env)
+
 #printf '\e[1;34m%-6s\e[m' "\ninstalling metallb... \n\n"
 
 printf "installing metallb... \n\n"
 
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+#kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+#kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
 
 printf "\n building images... \n\n"
 
-docker build -t nginx ./srcs/nginx
+docker build -t local-nginx ./srcs/nginx
 
 printf "\n running kubernetes cluster... \n\n"
 
@@ -17,4 +21,4 @@ kubectl apply -f ./srcs/nginx.yaml
 
 printf "\nopening dashboard... \n\n"
 
-minikube dashboard
+#minikube dashboard
