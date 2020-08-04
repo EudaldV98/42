ca6037ff2f9164af9bbfd0eda57c481edd5c5d59
Martin GARCIA LOPEZ <mgarcia-@e1r9p6.42.fr>
2020-08-04 18:21:34 +0200

    good luck amigo

--- ft_services/setup.sh
+++ ft_services/setup.sh
@@ -1,6 +1,6 @@
 #minikube delete
 
-#minikube config unset vm-driver
+minikube config unset vm-driver
 
 # Detect the platform (similar to $OSTYPE)
 OS="`uname`"
@@ -17,6 +17,8 @@ OS="`uname`"
 #	*) ;;
 #esac
 
+minikube start --driver=virtualbox --extra-config=apiserver.service-node-port-range=1-35000
+
 #IP=$(kubectl get node -o=custom-columns='DATA:status.addresses[0].address' | sed -n 2p)
 IP=$(minikube ip)
 
@@ -31,9 +33,10 @@ docker build -t mysql_alpine srcs/mysql
 kubectl apply -f srcs/k8s/mysql.yaml
 docker build -t nginx_alpine srcs/nginx
 docker build -t mysql_alpine srcs/mysql
-docker build -t phpmyadmin_alpine srcs/phpmyadmin
-docker build -t wordpress_alpine srcs/wordpress
+#docker build -t phpmyadmin_alpine srcs/phpmyadmin
+#docker build -t wordpress_alpine srcs/wordpress
 
+docker build -t nginx srcs/my_nginx
 docker build -t ftps --build-arg IP=${IP} srcs/ftps
 docker build -t influxdb srcs/influxdb
 docker build -t grafana srcs/grafana
@@ -41,8 +44,7 @@ docker build -t grafana srcs/grafana
 
 #deploys
 kubectl apply -f srcs/k8s/nginx.yaml
-
-kubectl apply -f srcs/k8s/nginx.yaml
+kubectl apply -f srcs/k8s/my_nginx.yaml
 kubectl apply -f srcs/k8s/ftps.yaml
 kubectl apply -f srcs/k8s/influxdb.yaml
 kubectl apply -f srcs/k8s/grafana.yaml
@@ -50,11 +52,11 @@ kubectl apply -f srcs/k8s/grafana.yaml
 minikube dashboard &
 
 
-sleep 20s
-kubectl exec -i $(kubectl get pods | grep mysql | cut -d" " -f1) -- mysql -u root -e 'CREATE DATABASE wordpress;' > /dev/null
-kubectl exec -i $(kubectl get pods | grep mysql | cut -d" " -f1) -- mysql wordpress -u root < srcs/mysql/srcs/wordpress.sql
+#sleep 20s
+#kubectl exec -i $(kubectl get pods | grep mysql | cut -d" " -f1) -- mysql -u root -e 'CREATE DATABASE wordpress;' > /dev/null
+#kubectl exec -i $(kubectl get pods | grep mysql | cut -d" " -f1) -- mysql wordpress -u root < srcs/mysql/srcs/wordpress.sql
 
 
-kubectl apply -f srcs/k8s/wordpress.yaml
-kubectl apply -f srcs/k8s/phpmyadmin.yaml
+#kubectl apply -f srcs/k8s/wordpress.yaml
+#kubectl apply -f srcs/k8s/phpmyadmin.yaml
 
