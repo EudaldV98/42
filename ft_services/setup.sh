601a428c82d64ae78b7b614857cea6c3be90635c
Martin GARCIA LOPEZ <mgarcia-@e1r9p2.42.fr>
2020-08-05 12:08:15 +0200

    alpine I don't like u bro

--- ft_services/setup.sh
+++ ft_services/setup.sh
@@ -1,25 +1,25 @@
-#minikube delete
-
+minikube delete
 minikube config unset vm-driver
 
 # Detect the platform (similar to $OSTYPE)
 OS="`uname`"
 
-#case $OS in
-#	"Linux")
-#		minikube start
-#		sed -i -e "s/xxxx-xxxx/172.17.0.10-172.17.0.15/g" srcs/configmap.yml
-#	;;
-#	"Darwin")
-#		minikube start --driver=virtualbox --extra-config=apiserver.service-node-port-range=1-35000
-#		sed -i -e "s/xxxx-xxxx/192.168.99.110-192.168.99.115/g" srcs/configmap.yml
-#	;;
-#	*) ;;
-#esac
+case $OS in
+	"Linux")
+		minikube start
+		sed -i -e "s/xxxx-xxxx/172.17.0.10-172.17.0.15/g" srcs/configmap.yml
+	;;
+	"Darwin")
+		minikube start --driver=virtualbox --extra-config=apiserver.service-node-port-range=1-35000
+		sed -i -e "s/xxxx-xxxx/192.168.99.110-192.168.99.110/g" srcs/configmap.yml
+	;;
+	*) ;;
+esac
 
-minikube start --driver=virtualbox --extra-config=apiserver.service-node-port-range=1-35000
+#minikube start --driver=virtualbox --extra-config=apiserver.service-node-port-range=1-35000
 
 #IP=$(kubectl get node -o=custom-columns='DATA:status.addresses[0].address' | sed -n 2p)
+
 IP=$(minikube ip)
 
 eval $(minikube docker-env)
@@ -29,10 +29,10 @@ kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.8.1/manifes
 kubectl create -f srcs/configmap.yml
 
 #builds
-docker build -t mysql_alpine srcs/mysql
-kubectl apply -f srcs/k8s/mysql.yaml
-docker build -t nginx_alpine srcs/nginx
-docker build -t mysql_alpine srcs/mysql
+#docker build -t mysql_alpine srcs/mysql
+#kubectl apply -f srcs/k8s/mysql.yaml
+#docker build -t nginx_alpine srcs/nginx
+#docker build -t mysql_alpine srcs/mysql
 #docker build -t phpmyadmin_alpine srcs/phpmyadmin
 #docker build -t wordpress_alpine srcs/wordpress
 
@@ -43,7 +43,7 @@ docker build -t grafana srcs/grafana
 
 
 #deploys
-kubectl apply -f srcs/k8s/nginx.yaml
+#kubectl apply -f srcs/k8s/nginx.yaml
 kubectl apply -f srcs/k8s/my_nginx.yaml
 kubectl apply -f srcs/k8s/ftps.yaml
 kubectl apply -f srcs/k8s/influxdb.yaml
--- ft_services/srcs/grafana/start.sh
+++ ft_services/srcs/grafana/start.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-# We run Telegraf and the Grafana web dashboard
-
 telegraf &
 cd ./grafana-6.7.2/bin/ && ./grafana-server
