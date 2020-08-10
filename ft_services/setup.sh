991a17839fd78b2e4c29b8d6027aaa9cf394aa01
corozco <corozco@student.42.fr>
2020-08-10 07:13:16 +0200

    changes wordpress + mysql / delete sleep

--- ft_services/setup.sh
+++ ft_services/setup.sh
@@ -4,21 +4,19 @@ minikube config unset vm-driver
 # Detect the platform (similar to $OSTYPE)
 OS="`uname`"
 
-#case $OS in
-#	"Linux")
-#		minikube start
-#		sed -i -e "s/xxxx-xxxx/172.17.0.10-172.17.0.15/g" srcs/configmap.yml
-#	;;
-#	"Darwin")
-#		minikube start --driver=virtualbox --extra-config=apiserver.service-node-port-range=1-35000
-#		sed -i -e "s/xxxx-xxxx/192.168.99.110-192.168.99.110/g" srcs/configmap.yml
-#	;;
-#	*) ;;
-#esac
-
-minikube start --driver=virtualbox
-
-IP=192.168.99.110
+case $OS in
+	"Linux")
+		minikube start
+		IP=172.17.0.10
+		sed -i -e "s/xxxx-xxxx/172.17.0.10-172.17.0.11/g" srcs/configmap.yml
+	;;
+	"Darwin")
+		minikube start --driver=virtualbox
+		IP=192.168.99.110
+		sed -i -e "s/xxxx-xxxx/192.168.99.110-192.168.99.111/g" srcs/configmap.yml
+	;;
+	*) ;;
+esac
 
 eval $(minikube docker-env)
 
@@ -30,30 +28,24 @@ kubectl create -f srcs/configmap.yml
 docker build -t mysql_alpine srcs/mysql
 kubectl apply -f srcs/k8s/mysql.yaml
 docker build -t nginx_alpine srcs/nginx
-docker build -t mysql_alpine srcs/mysql
 docker build -t phpmyadmin_alpine srcs/phpmyadmin
 docker build -t wordpress_alpine srcs/wordpress
-
 #docker build -t nginx srcs/my_nginx
 docker build -t ftps --build-arg IP=${IP} srcs/ftps
 docker build -t influxdb srcs/influxdb
 docker build -t grafana srcs/grafana
 
-
 #deploys
-kubectl apply -f srcs/k8s/nginx.yaml
 #kubectl apply -f srcs/k8s/my_nginx.yaml
+
+#minikube dashboard &
+
+kubectl exec -i $(kubectl get pods | grep mysql | cut -d" " -f1) -- mysql wordpress -u root < srcs/mysql/srcs/wordpress.sql
+
+kubectl apply -f srcs/k8s/nginx.yaml
 kubectl apply -f srcs/k8s/ftps.yaml
 kubectl apply -f srcs/k8s/influxdb.yaml
 kubectl apply -f srcs/k8s/grafana.yaml
-
-minikube dashboard &
-
-
-sleep 20s
-kubectl exec -i $(kubectl get pods | grep mysql | cut -d" " -f1) -- mysql -u root -e 'CREATE DATABASE wordpress;' > /dev/null
-kubectl exec -i $(kubectl get pods | grep mysql | cut -d" " -f1) -- mysql wordpress -u root < srcs/mysql/srcs/wordpress.sql
-
 
 kubectl apply -f srcs/k8s/wordpress.yaml
 kubectl apply -f srcs/k8s/phpmyadmin.yaml
--- ft_services/srcs/mysql/srcs/init.sh
+++ ft_services/srcs/mysql/srcs/init.sh
@@ -1,9 +1,9 @@
-#! /bin/bash
-mysql_install_db --user=root
-cat > sql << eof
+mysql_install_db --user=root --ldata=/var/lib/mysql
+cat > /tmp/sql << eof
+CREATE DATABASE wordpress;
 FLUSH PRIVILEGES;
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 eof
 telegraf &
-/usr/bin/mysqld --user=root --bootstrap --verbose=0 < sql
+/usr/bin/mysqld --console --init_file=/tmp/sql
